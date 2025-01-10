a1625102e6dc16174545bffe61723d87ebf31094
makandarSahil <sahilmakandar15@gmail.com>
2025-01-11 00:54:20 +0530

    link

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,30 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
-    <aside className="w-60 h-screen bg-gray-800 text-white shadow-md fixed left-0 top-0 bottom-0">
-      <div className="py-4 px-6">
-        <h1 className="text-2xl font-bold text-blue-400">Dashboard</h1>
+    <aside className='w-60 h-screen bg-gray-800 text-white shadow-md fixed left-0 top-0 bottom-0 py-8 px-1'>
+      <div className='py-4 px-6'>
+        <h1 className='text-2xl font-semibold text-blue-400'>Dashboard</h1>
       </div>
-      <nav className="mt-10">
+      <nav className='mt-4'>
         <ul>
-          <li className="group">
-            <div className="px-6 py-3 text-gray-400 group-hover:bg-gray-700 group-hover:text-white transition-all rounded-md">
-              <span className="text-lg font-medium">Teacher</span>
+          <li className='group'>
+            <div className='px-6 py-3 text-gray-400 group-hover:bg-gray-700 group-hover:text-white transition-all '>
+              <span className='text-lg font-medium'>Teacher</span>
             </div>
-            <ul className="ml-4 text-sm mt-1 space-y-1">
-              <li className="px-4 py-2 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md">
-                Teacher Master Form
-              </li>
-              <li className="px-4 py-2 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md">
-                Class Teacher Allotment
-              </li>
-              <li className="px-4 py-2 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md">
-                Teacher Allotment
-              </li>
-              <li className="px-4 py-2 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md">
-                Mentor Allotment
-              </li>
+            <ul className='ml-4 text-sm mt-1 space-y-1'>
+              <Link to=''>
+                <li className='px-4 py-2 text-gray-300 hover:bg-gray-700 hover:text-white'>
+                  Teacher Master Form
+                </li>
+              </Link>
+              <Link to=''>
+                <li className='px-4 py-2 text-gray-300 hover:bg-gray-700 hover:text-white'>
+                  Class Teacher Allotment
+                </li>
+              </Link>
+              <Link to=''>
+                <li className='px-4 py-2 text-gray-300 hover:bg-gray-700 hover:text-white'>
+                  Teacher Allotment
+                </li>
+              </Link>
+              <Link to=''>
+                <li className='px-4 py-2 text-gray-300 hover:bg-gray-700 hover:text-white'>
+                  Mentor Allotment
+                </li>
+              </Link>
             </ul>
           </li>
         </ul>
