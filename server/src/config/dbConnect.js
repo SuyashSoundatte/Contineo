440740876c8e2c95e5ecbccbcc7fd1504d26d634
SuyashSoundatte <suyashsoundatte7@gmail.com>
2025-01-15 19:35:16 +0530

    update

--- server/src/config/dbConnect.js
+++ server/src/config/dbConnect.js
@@ -5,7 +5,7 @@ dotenv.config()
 const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  server: process.env.DB_HOST,  // Ensure this is a string
+  server: process.env.DB_HOST, 
   database: process.env.DB_NAME,
   port: parseInt(process.env.DB_PORT),
   options: {
@@ -14,7 +14,6 @@ const config = {
   },
 };
 
-console.log(config); // Log the entire config object to ensure everything is correct.
 
 const poolPromise = new sql.ConnectionPool(config).connect();
 
