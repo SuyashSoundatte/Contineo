325fbdd99c2a99ac2962817f4b432751a522a7bb
makandarSahil <sahilmakandar15@gmail.com>
2025-01-16 15:29:24 +0530

    .

--- src/Forms/TeacherForm.jsx
+++ src/Forms/TeacherForm.jsx
@@ -57,7 +57,7 @@ const TeacherForm = () => {
     {
       name: 'Action',
       cell: (row) => (
-        <button className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-green-700">
+        <button className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
           Assign
         </button>
       ),
